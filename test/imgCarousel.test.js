5c3b11442c0f798565ccbdfd2d4d934d3ff4da06
Ray Lin <raylin009@gmail.com>
2019-04-08 16:53:10 -0700

    testing for StDescript component complete

--- test/imgCarousel.test.js
+++ test/imgCarousel.test.js
@@ -10,8 +10,18 @@ describe('Examining the syntax of Jest tests', () => {
      });
   });
 
-describe('test1', () => {
+describe('StDescript component', () => {
     it('renders without crashing', () => {
         shallow(<StDescipt descriptArr={["test1", "test2", "test3"]}/>);
+
       });
+    it('renders the right amount of bullet points', () => {
+        const testInput3 = ["bullet point 1", "bullet point 2", "bullet point 3"];
+        const wrapper = shallow(<StDescipt descriptArr={testInput3} />);
+        expect(wrapper.find('ul').children().length).toEqual(3);
+    });
+    it('render the correct text for the bullet point', () => {
+        const wrapper = shallow(<StDescipt descriptArr={["point made"]} />);
+        expect(wrapper.find('li').text()).toBe('point made');
+    })
 })
